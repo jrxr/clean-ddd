89d1c8b46a8b44aea0e7b69346a84c9cb621a0b6
agacy junior <juniormartinssom@gmail.com>
2023-05-21 13:24:21 -0300

    Refactoring unit tests

---
+++ test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,10 @@
+import { Question } from '@/domain/forum/application/enterprise/entities/question'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+
+export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  async create(question: Question) {
+    this.items.push(question)
+  }
+}
