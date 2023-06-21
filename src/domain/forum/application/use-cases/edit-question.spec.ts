2d397fc8a6116ce6166e27dd5ce426cafeeffc77
agacy junior <juniormartinssom@gmail.com>
2023-06-21 10:54:37 -0300

    Attachments in responses

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -12,7 +12,10 @@ let sut: EditQuestionUseCase
 
 describe('Edit Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionAttachmentsRepository()
     sut = new EditQuestionUseCase(
